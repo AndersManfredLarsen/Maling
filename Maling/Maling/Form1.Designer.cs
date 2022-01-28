ad6dd803c613d99c5dac9ba094a80bda7d9aaed9
KongzCringe <80678557+KongzCringe@users.noreply.github.com>
2022-01-28 15:28:17 +0100

    Basic Gui Menu

--- Maling/Maling/Form1.Designer.cs
+++ Maling/Maling/Form1.Designer.cs
@@ -29,13 +29,101 @@ namespace Maling
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.RedColorBtn = new System.Windows.Forms.Button();
+            this.GreenColorBtn = new System.Windows.Forms.Button();
+            this.BlueColorBtn = new System.Windows.Forms.Button();
+            this.YellowColorBtn = new System.Windows.Forms.Button();
+            this.BrushSize = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.BrushSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RedColorBtn
+            // 
+            this.RedColorBtn.BackColor = System.Drawing.Color.Red;
+            this.RedColorBtn.Location = new System.Drawing.Point(12, 10);
+            this.RedColorBtn.Name = "RedColorBtn";
+            this.RedColorBtn.Size = new System.Drawing.Size(59, 29);
+            this.RedColorBtn.TabIndex = 0;
+            this.RedColorBtn.Text = "Red";
+            this.RedColorBtn.UseVisualStyleBackColor = false;
+            // 
+            // GreenColorBtn
+            // 
+            this.GreenColorBtn.BackColor = System.Drawing.Color.Lime;
+            this.GreenColorBtn.Location = new System.Drawing.Point(77, 9);
+            this.GreenColorBtn.Name = "GreenColorBtn";
+            this.GreenColorBtn.Size = new System.Drawing.Size(58, 28);
+            this.GreenColorBtn.TabIndex = 1;
+            this.GreenColorBtn.Text = "Green";
+            this.GreenColorBtn.UseVisualStyleBackColor = false;
+            // 
+            // BlueColorBtn
+            // 
+            this.BlueColorBtn.BackColor = System.Drawing.Color.Blue;
+            this.BlueColorBtn.Location = new System.Drawing.Point(141, 10);
+            this.BlueColorBtn.Name = "BlueColorBtn";
+            this.BlueColorBtn.Size = new System.Drawing.Size(59, 27);
+            this.BlueColorBtn.TabIndex = 2;
+            this.BlueColorBtn.Text = "Blue";
+            this.BlueColorBtn.UseVisualStyleBackColor = false;
+            // 
+            // YellowColorBtn
+            // 
+            this.YellowColorBtn.BackColor = System.Drawing.Color.Yellow;
+            this.YellowColorBtn.Location = new System.Drawing.Point(206, 9);
+            this.YellowColorBtn.Name = "YellowColorBtn";
+            this.YellowColorBtn.Size = new System.Drawing.Size(59, 27);
+            this.YellowColorBtn.TabIndex = 3;
+            this.YellowColorBtn.Text = "Yellow";
+            this.YellowColorBtn.UseVisualStyleBackColor = false;
+            // 
+            // BrushSize
+            // 
+            this.BrushSize.LargeChange = 1;
+            this.BrushSize.Location = new System.Drawing.Point(315, 10);
+            this.BrushSize.Name = "BrushSize";
+            this.BrushSize.Size = new System.Drawing.Size(104, 45);
+            this.BrushSize.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Maling.Properties.Resources.support_turtle;
+            this.pictureBox1.Location = new System.Drawing.Point(271, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(38, 27);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BrushSize);
+            this.Controls.Add(this.YellowColorBtn);
+            this.Controls.Add(this.BlueColorBtn);
+            this.Controls.Add(this.GreenColorBtn);
+            this.Controls.Add(this.RedColorBtn);
+            this.Name = "Form1";
+            this.Text = "Backalley Paint";
+            ((System.ComponentModel.ISupportInitialize)(this.BrushSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button RedColorBtn;
+        private System.Windows.Forms.Button GreenColorBtn;
+        private System.Windows.Forms.Button BlueColorBtn;
+        private System.Windows.Forms.Button YellowColorBtn;
+        private System.Windows.Forms.TrackBar BrushSize;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
