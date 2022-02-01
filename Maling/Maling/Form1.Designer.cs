1c5e91aaaf3b6c57c6495f872690419b1107db1f
KongzCringe <80678557+KongzCringe@users.noreply.github.com>
2022-02-01 14:33:54 +0100

    Panel and Brush size slider

--- Maling/Maling/Form1.Designer.cs
+++ Maling/Maling/Form1.Designer.cs
@@ -34,9 +34,9 @@ namespace Maling
             this.BlueColorBtn = new System.Windows.Forms.Button();
             this.YellowColorBtn = new System.Windows.Forms.Button();
             this.BrushSize = new System.Windows.Forms.TrackBar();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.BrushSize)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // RedColorBtn
@@ -82,26 +82,35 @@ namespace Maling
             // BrushSize
             // 
             this.BrushSize.LargeChange = 1;
-            this.BrushSize.Location = new System.Drawing.Point(315, 10);
+            this.BrushSize.Location = new System.Drawing.Point(271, 23);
             this.BrushSize.Name = "BrushSize";
             this.BrushSize.Size = new System.Drawing.Size(104, 45);
             this.BrushSize.TabIndex = 4;
             // 
-            // pictureBox1
+            // label1
             // 
-            this.pictureBox1.Image = global::Maling.Properties.Resources.support_turtle;
-            this.pictureBox1.Location = new System.Drawing.Point(271, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(38, 27);
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(297, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Brush size";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(12, 61);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 377);
+            this.panel1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.BrushSize);
             this.Controls.Add(this.YellowColorBtn);
             this.Controls.Add(this.BlueColorBtn);
@@ -110,7 +119,6 @@ namespace Maling
             this.Name = "Form1";
             this.Text = "Backalley Paint";
             ((System.ComponentModel.ISupportInitialize)(this.BrushSize)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,7 +131,8 @@ namespace Maling
         private System.Windows.Forms.Button BlueColorBtn;
         private System.Windows.Forms.Button YellowColorBtn;
         private System.Windows.Forms.TrackBar BrushSize;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
