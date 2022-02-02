39c37196896f56d5617324d50e8778d9242195ca
Anders Manfred Larsen <anders18018@gmail.com>
2022-02-02 12:51:03 +0100

    Color change and brushsize

--- Maling/Maling/Form1.Designer.cs
+++ Maling/Maling/Form1.Designer.cs
@@ -37,72 +37,85 @@ namespace Maling
             this.label1 = new System.Windows.Forms.Label();
             this.Drawing = new System.Windows.Forms.Panel();
             this.SaveButton = new System.Windows.Forms.Button();
+            this.EraserButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.BrushSize)).BeginInit();
             this.SuspendLayout();
             // 
             // RedColorBtn
             // 
             this.RedColorBtn.BackColor = System.Drawing.Color.Red;
-            this.RedColorBtn.Location = new System.Drawing.Point(12, 12);
+            this.RedColorBtn.Location = new System.Drawing.Point(16, 15);
+            this.RedColorBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.RedColorBtn.Name = "RedColorBtn";
-            this.RedColorBtn.Size = new System.Drawing.Size(60, 30);
+            this.RedColorBtn.Size = new System.Drawing.Size(80, 37);
             this.RedColorBtn.TabIndex = 0;
             this.RedColorBtn.Text = "Red";
             this.RedColorBtn.UseVisualStyleBackColor = false;
+            this.RedColorBtn.Click += new System.EventHandler(this.RedColorBtn_Click);
             // 
             // GreenColorBtn
             // 
             this.GreenColorBtn.BackColor = System.Drawing.Color.Lime;
-            this.GreenColorBtn.Location = new System.Drawing.Point(77, 12);
+            this.GreenColorBtn.Location = new System.Drawing.Point(103, 15);
+            this.GreenColorBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.GreenColorBtn.Name = "GreenColorBtn";
-            this.GreenColorBtn.Size = new System.Drawing.Size(60, 30);
+            this.GreenColorBtn.Size = new System.Drawing.Size(80, 37);
             this.GreenColorBtn.TabIndex = 1;
             this.GreenColorBtn.Text = "Green";
             this.GreenColorBtn.UseVisualStyleBackColor = false;
+            this.GreenColorBtn.Click += new System.EventHandler(this.GreenColorBtn_Click);
             // 
             // BlueColorBtn
             // 
             this.BlueColorBtn.BackColor = System.Drawing.Color.Blue;
-            this.BlueColorBtn.Location = new System.Drawing.Point(141, 12);
+            this.BlueColorBtn.Location = new System.Drawing.Point(188, 15);
+            this.BlueColorBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BlueColorBtn.Name = "BlueColorBtn";
-            this.BlueColorBtn.Size = new System.Drawing.Size(60, 30);
+            this.BlueColorBtn.Size = new System.Drawing.Size(80, 37);
             this.BlueColorBtn.TabIndex = 2;
             this.BlueColorBtn.Text = "Blue";
             this.BlueColorBtn.UseVisualStyleBackColor = false;
+            this.BlueColorBtn.Click += new System.EventHandler(this.BlueColorBtn_Click);
             // 
             // YellowColorBtn
             // 
             this.YellowColorBtn.BackColor = System.Drawing.Color.Yellow;
-            this.YellowColorBtn.Location = new System.Drawing.Point(206, 12);
+            this.YellowColorBtn.Location = new System.Drawing.Point(275, 15);
+            this.YellowColorBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.YellowColorBtn.Name = "YellowColorBtn";
-            this.YellowColorBtn.Size = new System.Drawing.Size(60, 30);
+            this.YellowColorBtn.Size = new System.Drawing.Size(80, 37);
             this.YellowColorBtn.TabIndex = 3;
             this.YellowColorBtn.Text = "Yellow";
             this.YellowColorBtn.UseVisualStyleBackColor = false;
+            this.YellowColorBtn.Click += new System.EventHandler(this.YellowColorBtn_Click);
             // 
             // BrushSize
             // 
             this.BrushSize.LargeChange = 1;
-            this.BrushSize.Location = new System.Drawing.Point(271, 23);
+            this.BrushSize.Location = new System.Drawing.Point(361, 28);
+            this.BrushSize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BrushSize.Name = "BrushSize";
-            this.BrushSize.Size = new System.Drawing.Size(104, 45);
+            this.BrushSize.Size = new System.Drawing.Size(139, 56);
             this.BrushSize.TabIndex = 4;
+            this.BrushSize.Scroll += new System.EventHandler(this.BrushSize_Scroll);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(297, 7);
+            this.label1.Location = new System.Drawing.Point(396, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.Size = new System.Drawing.Size(74, 17);
             this.label1.TabIndex = 5;
             this.label1.Text = "Brush size";
             // 
             // Drawing
             // 
             this.Drawing.BackColor = System.Drawing.Color.White;
-            this.Drawing.Location = new System.Drawing.Point(12, 61);
+            this.Drawing.Location = new System.Drawing.Point(16, 75);
+            this.Drawing.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Drawing.Name = "Drawing";
-            this.Drawing.Size = new System.Drawing.Size(776, 377);
+            this.Drawing.Size = new System.Drawing.Size(1035, 464);
             this.Drawing.TabIndex = 6;
             this.Drawing.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             this.Drawing.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
@@ -110,19 +123,31 @@ namespace Maling
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(728, 12);
+            this.SaveButton.Location = new System.Drawing.Point(971, 15);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(60, 30);
+            this.SaveButton.Size = new System.Drawing.Size(80, 37);
             this.SaveButton.TabIndex = 7;
             this.SaveButton.Text = "Save";
             this.SaveButton.UseVisualStyleBackColor = true;
             this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
             // 
+            // EraserButton
+            // 
+            this.EraserButton.Location = new System.Drawing.Point(519, 15);
+            this.EraserButton.Name = "EraserButton";
+            this.EraserButton.Size = new System.Drawing.Size(79, 37);
+            this.EraserButton.TabIndex = 8;
+            this.EraserButton.Text = "Eraser";
+            this.EraserButton.UseVisualStyleBackColor = true;
+            this.EraserButton.Click += new System.EventHandler(this.EraserButton_Click);
+            // 
             // GeneralWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.EraserButton);
             this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.Drawing);
             this.Controls.Add(this.label1);
@@ -132,6 +157,7 @@ namespace Maling
             this.Controls.Add(this.GreenColorBtn);
             this.Controls.Add(this.RedColorBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "GeneralWindow";
             this.Text = "Backalley Paint";
             this.Load += new System.EventHandler(this.GeneralWindow_Load);
@@ -151,6 +177,7 @@ namespace Maling
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel Drawing;
         private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button EraserButton;
     }
 }
 
--- Maling/Maling/Form1.cs
+++ Maling/Maling/Form1.cs
@@ -17,11 +17,19 @@ namespace Maling
         int y = -1;
         bool moving = false;
         Pen pen;
+        Color newColor;
+        int width;
+
         public GeneralWindow()
         {
             InitializeComponent();
             g = Drawing.CreateGraphics();
+
             pen = new Pen(Color.Black, 5);
+
+            newColor = Color.Black;
+            width = 5;
+            BrushSize.Value = 5;
         }
 
         Bitmap drawingField;
@@ -46,6 +54,8 @@ namespace Maling
         }
         private void panel1_MouseMove(object sender, MouseEventArgs e)
         {
+            pen.Color = newColor;
+            pen.Width = width;
             if (moving && x != -1 && y != -1)
             {
                 g.DrawLine(pen, new Point(x, y), e.Location);
@@ -60,6 +70,36 @@ namespace Maling
             x = -1;
             y = -1;
 
+        }
+
+        private void RedColorBtn_Click(object sender, EventArgs e)
+        {
+            newColor = Color.Red;
+        }
+
+        private void GreenColorBtn_Click(object sender, EventArgs e)
+        {
+            newColor = Color.Green;
+        }
+
+        private void BlueColorBtn_Click(object sender, EventArgs e)
+        {
+            newColor = Color.Blue;
+        }
+
+        private void YellowColorBtn_Click(object sender, EventArgs e)
+        {
+            newColor = Color.Yellow;
+        }
+
+        private void EraserButton_Click(object sender, EventArgs e)
+        {
+            newColor = Color.White;
+        }
+
+        private void BrushSize_Scroll(object sender, EventArgs e)
+        {
+            width = BrushSize.Value;
         }
     }
 }
